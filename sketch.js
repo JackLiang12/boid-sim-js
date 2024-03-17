45435c9c19a4f1d87238bc28bcd430d9b9f81132
JackLiang12 <156202662+JackLiang12@users.noreply.github.com>
2024-03-17 14:40:39 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const flock = [];
-
+const obstacles = [];
 let alignSlider, cohesionSlider, seperationSlider;
 function setup(){
     createCanvas(1280, 720);
@@ -15,8 +15,15 @@ function draw(){
     background(51);
     for (let boid of flock ){
         boid.edges();
-        boid.flock(flock);
+        boid.flock(flock,obstacles);
         boid.update();
         boid.show();
     }
+    for (let obstacle of obstacles) {
+        obstacle.show();
+      }
+
 }
+function mouseClicked() {
+    obstacles.push(new obstacle(mouseX, mouseY));
+  }
